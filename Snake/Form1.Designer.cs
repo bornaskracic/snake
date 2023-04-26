848c35de17ad21334b726f630b8af23de1e29589
bornaskracic <bskracic@racunarstvo.hr>
2023-04-26 14:43:47 +0200

    set up basic workfklow

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -29,11 +29,50 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.snake = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // snake
+            // 
+            this.snake.Location = new System.Drawing.Point(351, 181);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(50, 50);
+            this.snake.TabIndex = 0;
+            this.snake.UseVisualStyleBackColor = true;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 500;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.snake);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 425);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button snake;
+        private System.Windows.Forms.Timer gameTimer;
+        private Panel panel1;
     }
 }
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -2,9 +2,38 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        private Button fruit;
+
         public Form1()
         {
             InitializeComponent();
+            SpawnFruit();
+            
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            MoveSnake();
+        }
+
+        private void MoveSnake()
+        {
+            CheckBoundaries();
+            if (snake.Bounds.IntersectsWith(fruit.Bounds))
+                SpawnFruit();
+
+            // Move logic
+
+        }
+
+        private void CheckBoundaries()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SpawnFruit()
+        {
+            throw new NotImplementedException();
         }
     }
 }
